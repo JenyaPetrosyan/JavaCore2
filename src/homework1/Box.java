0909904b92623ce9fe7dd0a068845e77772382ba
JenyaPetrosan <petrosyan.jenya@bk.ru>
2021-10-26 21:36:05 +0400

    Գլուխ 6-ի օրինակները

---
+++ src/homework1/Box.java
@@ -0,0 +1,13 @@
+package homework1;
+
+public class Box {
+    double width;
+    double height;
+    double depht;
+
+    void volume() {
+        System.out.print( "volume:");
+        System.out.println(width * height * depht);
+    }
+}
+
---
+++ src/homework1/BoxDemo.java
@@ -0,0 +1,23 @@
+package homework1;
+
+public class BoxDemo {
+    public static void main(String[] args) {
+        Box MyBox = new Box();
+        Box MyBox1 = new Box();
+        double vol;
+        MyBox.width = 10;
+        MyBox.height = 20;
+        MyBox.depht = 15;
+
+        vol = MyBox.depht * MyBox.height * MyBox.depht;
+        System.out.println(vol);
+
+        MyBox1.width = 3;
+        MyBox1.height = 6;
+        MyBox1.depht = 9;
+
+        vol= MyBox1.depht * MyBox1.height * MyBox1.width;
+        System.out.println();
+
+    }
+}
